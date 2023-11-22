574c7be8174045a58d61a0acc4027605a3c5b8d7
Kartoshka <kartoshka0811@gmail.com>
2023-11-23 01:12:47 +0300

    upgrade

--- src/Main.java
+++ src/Main.java
@@ -6,53 +6,21 @@ import java.util.concurrent.*;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        String[] texts = new String[5];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
 
-        Runnable logic = () -> {
-            String[] texts = new String[5];
-            for (int i = 0; i < texts.length; i++) {
-                texts[i] = generateText("aab", 30_000);
-            }
-            for (String text : texts) {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            }
-        };
         long startTs = System.currentTimeMillis(); // start time
 
-        List<Thread> threads = new ArrayList<>();
-        Thread thread1 = new Thread(logic);
-        Thread thread2 = new Thread(logic);
-        Thread thread3 = new Thread(logic);
-        Thread thread4 = new Thread(logic);
-        Thread thread5 = new Thread(logic);
-        threads.add(thread1);
-        threads.add(thread2);
-        threads.add(thread3);
-        threads.add(thread4);
-        threads.add(thread5);
+        for (String text : texts){
+            threads.add(threadLogic(text));
+        }
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
+        for (Thread thread : threads){
+            thread.start();
+        }
 
         for (Thread thread : threads) {
             thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
@@ -60,6 +28,32 @@ public class Main {
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
 
+    }
+
+    public static Thread threadLogic(String text){
+        return new Thread(
+                () -> {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
+                            }
+                        }
+                    }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                }
+        );
     }
 
     public static String generateText(String letters, int length) {
